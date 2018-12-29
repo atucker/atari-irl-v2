492feac51f17d4e28a496718e5d2dc00210d3c1f
Aaron Tucker <aaron@wave.com>
2018-12-29 02:15:23 -0500

    flesh things out with a sampler and random policy. I'm worried that I made up a kind of meh abstraction for handling the fact that we'll want to store different information in the batches depending on the RL algorithm, but this seems kind of reasonable -- the policy definitely needs to tell us the actions, and can write down whatever it will want for training later

--- atari-irl/irl.py
+++ atari-irl/irl.py
@@ -1,37 +1,119 @@
-from typing import NamedTuple, Tuple
+from typing import NamedTuple, Tuple, Any, Dict, List
+from collections import OrderedDict
 import numpy as np
 import gym
 
 
+class TimeShape(Tuple[int]):
+    @property
+    def T(self):
+        return self[-1]
+    @property
+    def num_envs(self):
+        return self[-2]
+
+
 class Observations(NamedTuple):
-    shape: Tuple[int]
+    time_shape: TimeShape
     obs: np.ndarray
 
 
-class Batch(NamedTuple):
-    shape: Tuple[int]
+class Actions(NamedTuple):
+    time_shape: TimeShape
+    actions: np.ndarray
+
+
+class Rewards(NamedTuple):
+    time_shape: TimeShape
+    rewards: np.ndarray
+
+
+class EnvInfo(NamedTuple):
+    time_shape: TimeShape
+
+    # These come from the gym Environment interface
     obs: np.ndarray
-    acts: np.ndarray
     rewards: np.ndarray
     dones: np.ndarray
 
+    epinfos: List[Dict[str, Any]]
+
+
+class PolicyInfo(NamedTuple):
+    time_shape: TimeShape
+    actions: np.ndarray
+
+
+class Stacker:
+    def __init__(self, other_cls: NamedTuple) -> None:
+        self.data_cls = other_cls
+        self.data = OrderedDict((f, []) for f in self.data_cls._fields)
+
+    def add(self, tup: NamedTuple) -> None:
+        assert isinstance(tup, self.data_cls)
+        for f in tup._fields:
+            self.data[f].append(getattr(tup, f))
+
+
+class Batch(NamedTuple):
+    time_shape: TimeShape
+    env_info: EnvInfo
+    policy_info: PolicyInfo
+
+    @property
+    def obs(self):
+        return self.env_info.obs
+
+    @property
+    def acts(self):
+        return self.policy_info.actions
+
+    @property
+    def rewards(self):
+        return self.env_info.rewards
+
 
 class Buffer(NamedTuple):
-    shape: Tuple[int]
+    time_shape: TimeShape
     obs: np.ndarray
     acts: np.ndarray
     rewards: np.ndarray
 
 
 class Policy:
-    def __init__(self, obs_space: Tuple[int], act_space: Tuple[int]) -> None:
-        raise NotImplemented
+    def __init__(self, obs_space: Tuple[int], act_space: gym.Space) -> None:
+        self.obs_space = obs_space
+        self.act_space = act_space
 
-    def get_actions(self, obs_batch: Observations) -> Batch:
+    def get_actions(self, obs_batch: Observations) -> Tuple[Actions, Any]:
         raise NotImplemented
 
     def train(self, buffer: Buffer) -> None:
         raise NotImplemented
+
+
+class RandomPolicy(Policy):
+    def get_actions(self, obs: Observations) -> Tuple[Actions, Any]:
+        if len(obs.time_shape) == 1:
+            return Actions(
+                time_shape=obs.time_shape,
+                actions=np.array([
+                    self.act_space.sample() for _ in range(obs.time_shape.T)
+                ])
+            ), None
+        elif len(obs.time_shape) == 2:
+            return Actions(
+                time_shape=obs.time_shape,
+                actions=np.array([
+                    [self.act_space.sample() for _ in range(obs.time_shape.num_envs)]
+                    for _ in range(obs.time_shape.T)
+                ])
+            ), None
+        else:
+            raise NotImplemented
+
+    def train(self, buffer: Buffer) -> None:
+        pass
 
 
 class RewardModel:
@@ -46,16 +128,50 @@ class RewardModel:
 
 
 class Sampler:
-    def __init__(self, env: gym.Env, policy: Policy, rollout_t: int) -> None:
+    def __init__(self, env: gym.Env, policy: Policy) -> None:
         self.env = env
+        self.num_envs = env.num_envs
         self.policy = policy
-        self.rollout_t = rollout_t
-
-    def sample_batch(self, rollout_t: int) -> Batch:
-        pass
+        self.obs = None
+        self.reset()
 
     def reset(self) -> None:
-        self.env.reset()
+        self.obs = self.env.reset()
+
+    def sample_batch(self, rollout_t: int, show=False) -> Batch:
+        assert self.obs is not None, "Need to call reset"
+
+        time_shape = TimeShape((self.num_envs, rollout_t))
+
+        env_info_stacker = Stacker[EnvInfo]()
+        policy_info_stacker = Stacker[PolicyInfo]()
+
+        for _ in range(rollout_t):
+            policy_step = self.policy.get_actions(self.obs)
+            policy_info_stacker.append(policy_step)
+
+            self.obs[:], rewards, self.dones, epinfos = self.env.step(policy_step.actions)
+            env_info_stacker.append(EnvInfo(
+                time_shape=TimeShape((self.num_envs, 1)),
+                obs=self.obs.copy(),
+                rewards=rewards,
+                dones=self.dones,
+                epinfos=epinfos
+            ))
+
+        return Batch(
+            env_info=EnvInfo(
+                time_shape=time_shape,
+                obs=np.array(env_info_stacker.obs),
+                rewards=np.array(env_info_stacker.rewards),
+                dones=np.array(env_info_stacker.dones),
+                epinfos=[_ for _ in env_info_stacker.epinfos if _]
+            ),
+            policy_info=PolicyInfo(
+                time_shape=time_shape,
+                actions=np.array(policy_info_stacker.actions)
+            )
+        )
 
 
 class DummyBuffer:
@@ -68,10 +184,19 @@ class DummyBuffer:
 
 class IRL:
     def __init__(self, args):
-        pass
+        self.env = gym.make('PongNoFrameskip-v4')
+        self.policy = RandomPolicy(
+            obs_space=self.env.observation_space,
+            act_space=self.env.action_space
+        )
+        self.sampler = Sampler(
+            env=self.env,
+            policy=self.policy
+        )
+
 
     def obtain_samples(self):
-        pass
+        return self.sampler.sample_batch(128)
 
     def update_buffer(self):
         pass
@@ -84,7 +209,10 @@ class IRL:
 
     def train(self):
         samples = self.obtain_samples()
-        buffer  = self.update_buffer(samples)
-        buffer  = self.update_discriminator(buffer)
-        self.update_policy(buffer)
+        import pdb; pdb.set_trace()
+        #buffer  = self.update_buffer(samples)
+        #buffer  = self.update_discriminator(buffer)
+        #self.update_policy(buffer)
 
+
+IRL(None).train()
