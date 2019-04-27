fix logprob calculation to work even if it's not in policy_info

atari_irl/discriminators.py
@@ -248,9 +248,11 @@ class AtariAIRL:
         obs: np.ndarray,
         acts: np.ndarray,
         next_obs: Optional[np.ndarray]=None,
-        log_probs: Optional[np.ndarray]=None,
+        lprobs: Optional[np.ndarray]=None,
         **kwargs
     ) -> np.ndarray:
+        if isinstance(acts, list):
+            acts = np.array(acts)
         if len(acts.shape) == 1:
             acts = one_hot(acts, self.dU)
         if self.score_discrim:
@@ -258,7 +260,7 @@ class AtariAIRL:
                 self.modify_obs(obs),
                 self.modify_obs(next_obs),
                 acts,
-                log_probs
+                lprobs
             )
             path_probs = np.expand_dims(path_probs, axis=1)
             scores = tf.get_default_session().run(

atari_irl/headers.py
@@ -249,24 +249,31 @@ class Buffer(Generic[T]):
                 print(f"{key}: {ans.shape}")
             assert len(ans) > 0
 
+            if 'act' in key:
+                ans = np.array(ans)
             sampled_keys[key] = ans
             return ans
+
+        def compute_logprobs():
+            get_key('obs')
+            get_key('acts')
+            sampled_keys['lprobs'] = self.policy.get_a_logprobs(
+                obs=sampled_keys['obs'],
+                acts=sampled_keys['acts']
+            )
+
         
         for key in keys:
             if key not in ['lprobs', 'rewards']:
                 get_key(key)
         
         if self.overwrite_logprobs:
-            get_key('obs')
-            get_key('acts')
             if 'lprobs' in keys:
-                sampled_keys['lprobs'] = self.policy.get_a_logprobs(
-                    obs=sampled_keys['obs'],
-                    acts=sampled_keys['acts']
-                )
-                
+                compute_logprobs()
+
         if self.overwrite_rewards:
             if 'rewards' in keys:
+                compute_logprobs()
                 sampled_keys['rewards'] = self.discriminator.eval(**sampled_keys)
 
         ans = tuple(get_key(key) for key in keys)